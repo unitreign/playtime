8ae6586d1fccec5fde01e8ff53dc0ffa367adb2b
reign <RY8GN@proton.me>
2026-09-13 18:05:43 +0700

    cleaned

--- bin/playtime-uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Remove PlayTime hooks from EmulationStation scripts before deleting the app.
-set -eu
-
-APPDIR="$(CDPATH='' cd "$(dirname "$0")/.." && pwd)"
-export CFW=KNULLI
-exec "$APPDIR/playtime" uninstall "$@"
--- hooks/game-end.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Called by EmulationStation/Knulli when game exits.
-# $1=system  $2=rom_path  $3=rom_name
-set -eu
-APPDIR="$(CDPATH='' cd "$(dirname "$0")/.." && pwd)"
-"$APPDIR/playtime" session end "$1" "$2" "$3"
